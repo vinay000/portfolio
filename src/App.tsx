df318c19c31b2d589e7486b37b1b8863f8e364d9
vinay000 <vky11504119@gmail.com>
2026-05-07 17:00:07 +0530

    chore: add dynamic basename to Router for subpath hosting

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import RootRoutes from './routes/RootRoutes';
 import { ReactLenis } from '@studio-freight/react-lenis';
 import { CustomCursor } from './components/CustomCursor';
 import { TeslaGridCanvas } from './components/TeslaGridCanvas';
+import { WordGameOverlay } from './components/WordGameOverlay';
 
 const App: React.FC = () => {
   return (
     <ReactLenis root>
-      <Router>
+      <Router basename={import.meta.env.BASE_URL}>
         <CustomCursor />
         <TeslaGridCanvas />
-        <RootRoutes />
+        <div id="portfolio-content" className="w-full min-h-screen">
+          <RootRoutes />
+        </div>
+        <WordGameOverlay />
       </Router>
     </ReactLenis>
   );
